917bb079d909b5fa133f565b5406bdbe73695fd4
Thomas Pfau <thomas.pfau@aalto.fi>
2026-06-29 12:05:43 +0300

    Update init and gitignore

---
+++ slurm2sql/__init__.py
@@ -0,0 +1,2 @@
+""" This is the Slurm2sql package. 
+It contains the main slurm2sql module, as well as the database models used. """
--- slurm2sql/models/__init__.py
+++ slurm2sql/models/__init__.py
@@ -1,2 +1,3 @@
+""" Table and View definitions for Slurm2sql"""
 from slurm2sql.models.tables import MetaSlurmLastUpdate, Slurm
 from slurm2sql.models.views import Eff, Step, Allocation
